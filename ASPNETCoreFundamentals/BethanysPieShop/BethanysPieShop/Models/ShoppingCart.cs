d01e4c703d60564162982fe8e0ca1306ec36b325
Aman <a.shalginbayev@yandex.com>
2025-11-17 08:23:44 +0500

    Missing members

--- ASPNETCoreFundamentals/BethanysPieShop/BethanysPieShop/Models/ShoppingCart.cs
+++ ASPNETCoreFundamentals/BethanysPieShop/BethanysPieShop/Models/ShoppingCart.cs
@@ -2,5 +2,30 @@ namespace BethanysPieShop.Models;
 
 public class ShoppingCart : IShoppingCart
 {
-    
+    public void AddToCart(Pie pie)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int RemoveFromCart(Pie pie)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<ShoppingCartItem> GetShoppingCartItems()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ClearCart()
+    {
+        throw new NotImplementedException();
+    }
+
+    public decimal GetShoppingCartTotal()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<ShoppingCartItem> ShoppingCartItems { get; set; }
 }
